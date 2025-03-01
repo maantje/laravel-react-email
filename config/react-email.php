f2d63919c309c36d80eca85448377f6d45ea6f3d
Jamie Schouten <jamie@qlic.nl>
2025-03-01 22:34:22 +0100

    remove line break

--- config/react-email.php
+++ config/react-email.php
@@ -3,6 +3,5 @@
 return [
     'template_directory' => base_path(env('REACT_EMAIL_DIRECTORY')),
     'node_path' => env('REACT_EMAIL_NODE_PATH'),
-
     'tsx_path' => env('REACT_EMAIL_TSX_PATH'),
 ];
